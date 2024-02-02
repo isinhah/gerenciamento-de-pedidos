025fd1dc94f93dc44a4b84e0fe1153f61fc88966
Isabel Henrique <6isinha6@gmail.com>
2024-02-02 16:48:00 -0300

    OrderStatus enum

---
+++ src/main/java/com/dev/course/entities/enums/OrderStatus.java
@@ -0,0 +1,29 @@
+package com.dev.course.entities.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT(1),
+    PAID(2),
+    SHIPPED(3),
+    DELIVERED(4),
+    CANCELED(5);
+
+    private int code;
+
+    private OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    //Converte o valor numerico para tipo enumerado: Vai retornar o orderstatus correspondente ao codigo existente
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus value : OrderStatus.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid OrderStatus code. ");
+    }
+}
