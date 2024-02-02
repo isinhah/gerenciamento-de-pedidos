36a2410dfe9b9dfd7c401c04db92d68b20339eb2
Isabel Henrique <6isinha6@gmail.com>
2024-02-02 14:04:55 -0300

    entidade Order - Parte 1

---
+++ src/main/java/com/dev/course/entities/Order.java
@@ -0,0 +1,64 @@
+package com.dev.course.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+    @ManyToOne //Muitos: pedidos - Um: usuario
+    @JoinColumn(name = "client_id") //Nome da chave estrangeira
+    private User client;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment) {
+        this.id = id;
+        this.moment = moment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
