4617f89b0dbd10ba308747cbd1d51a751b9f7131
Isabel Henrique <6isinha6@gmail.com>
2024-02-05 18:46:33 -0300

    associação OneToOne entre Payment e Order

--- src/main/java/com/dev/course/entities/Order.java
+++ src/main/java/com/dev/course/entities/Order.java
@@ -27,6 +27,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL) //As duas entidades em OneToOne vão ter o mesmo id
+    private Payment payment;
+
     public Order() {
     }
 
@@ -68,6 +71,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems() {
