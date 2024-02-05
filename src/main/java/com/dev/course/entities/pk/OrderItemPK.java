0ce223464776c986ab460c59fda0d96d065a32bf
Isabel Henrique <6isinha6@gmail.com>
2024-02-05 12:45:58 -0300

    entidade OrderItem, associação com atributos extras - Parte 1

---
+++ src/main/java/com/dev/course/entities/pk/OrderItemPK.java
@@ -0,0 +1,50 @@
+package com.dev.course.entities.pk;
+
+import com.dev.course.entities.Order;
+import com.dev.course.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+//Classe auxiliar: chave primária composta (entre produto e pedido)
+@Embeddable
+public class OrderItemPK implements Serializable {
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(product, that.product) && Objects.equals(order, that.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(product, order);
+    }
+}
