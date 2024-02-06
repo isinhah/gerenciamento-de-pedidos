eee6b510b9658d2134b15dbb1491f28de6f23d2a
Isabel Henrique <6isinha6@gmail.com>
2024-02-06 13:53:55 -0300

    atualização do User

--- src/main/java/com/dev/course/services/UserService.java
+++ src/main/java/com/dev/course/services/UserService.java
@@ -30,4 +30,17 @@ public class UserService {
     public void delete(Long id) {
         userRepository.deleteById(id);
     }
+
+    public User update(Long id, User obj) {
+        User entity = userRepository.getReferenceById(id);
+        updateData(entity, obj);
+        return userRepository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        //Atualizar os dados do entity
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
 }
